d3a468c9779b1c22ea16424ee6ed1c3f1ecfdfd0
Nikki de Vries <nikki.devries7@gmail.com>
2023-11-03 11:15:49 -0500

    added functions. should be done by this point

--- wrangle.py
+++ wrangle.py
@@ -5,6 +5,8 @@ import env
 import os
 
 
+from sklearn.model_selection import train_test_split
+
 #-----------------------------------------------------------------#
 #Aquire functions:
 
@@ -70,3 +72,69 @@ def get_zillow_data():
 
 #-----------------------------------------------------------------#
 # Data prepation functions: 
+
+def create_zillow_data():
+    '''
+    This function acquires zillow.csv it is available
+    otherwise, it makes the SQL connection and uses the query provided
+    to read in the dataframe from SQL.
+    If they csv is not present, it will write one.
+    '''
+    filename = "zillow_2017.csv"
+
+    if os.path.isfile(filename):
+
+        return pd.read_csv(filename, index_col=0)
+    else:
+        # Create the url
+        db = 'zillow'
+        url = f'mysql+pymysql://{env.user}:{env.password}@{env.host}/{db}'
+        
+        sql_query = '''
+            SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips
+            FROM properties_2017
+            WHERE propertylandusetypeid = 261'''
+
+        # Read the SQL query into a dataframe
+        df = pd.read_sql(sql_query, url)
+
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename)
+
+        # Return the dataframe to the calling code
+        return df
+
+def prep_zillow(df):
+    '''
+    This function takes in a dataframe
+    renames the columns and drops nulls values
+    Additionally it changes datatypes for appropriate columns
+    and renames fips to actual county names.
+    Then returns a cleaned dataframe
+    '''
+    df = df.rename(columns = {'bedroomcnt':'bedrooms',
+                     'bathroomcnt':'bathrooms',
+                     'calculatedfinishedsquarefeet':'area',
+                     'taxvaluedollarcnt':'taxvalue',
+                     'fips':'county'})
+    
+    df = df.dropna()
+    
+    make_ints = ['bedrooms','area','taxvalue','yearbuilt']
+
+    for col in make_ints:
+        df[col] = df[col].astype(int)
+        
+    df.county = df.county.map({6037:'LA',6059:'Orange',6111:'Ventura'})
+    
+    return df
+
+def split_zillow_data(df):
+    '''
+    This funciton will split the data frame into train, validate, and test
+    '''
+    
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+    
+    return train, validate, test
